d9b37326821a373961f78dcffe72a749a25ae85a
william koller <wkoller25@gmail.com>
2021-01-20 01:02:00 -0300

    fgeat: add AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configService } from './config/config.service';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig()[0]), forwardRef(() => UserModule)],
+  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig()[0]), forwardRef(() => UserModule), AuthModule],
   controllers: [],
   providers: [],
 })
