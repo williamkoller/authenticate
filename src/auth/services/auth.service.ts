28fe3e32e4fa40ee9aaa599fe47521a4615464fc
william koller <wkoller25@gmail.com>
2021-01-20 01:23:40 -0300

    feat: add validateUser in AuthService

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/entities/user.entity';
+import { UserService } from 'src/user/services/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.userService.findOne(username);
+    if (user && user.password === password) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+}
