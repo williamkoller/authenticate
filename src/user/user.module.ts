f5fae100022fe1039acfca2f2eb628549cdc424f
william koller <wkoller25@gmail.com>
2021-01-20 00:54:41 -0300

    feat: set UserRepository in UserModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserService } from './services/user.service';
+import { UserController } from './controllers/user.controller';
+import { UserRepository } from './repositories/user.repository';
 import { User } from 'src/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, UserRepository])],
+  providers: [UserService],
+  controllers: [UserController],
+  exports: [UserService],
 })
 export class UserModule {}
