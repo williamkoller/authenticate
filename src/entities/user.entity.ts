a28438a9630b983dc2502ebe8894757cab20f52f
william koller <wkoller25@gmail.com>
2021-01-20 00:53:22 -0300

    refactor: set type column in user entity

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,23 +1,18 @@
-import {
-  Column,
-  CreateDateColumn,
-  Entity,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-} from 'typeorm';
+import { hashPasswordTransform } from 'src/common/helpers/bcrypt';
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity('users')
 export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column()
+  @Column({ type: 'varchar', nullable: false })
   username: string;
 
-  @Column()
+  @Column({ type: 'varchar', nullable: false })
   email: string;
 
-  @Column()
+  @Column({ transformer: hashPasswordTransform, select: false })
   password: string;
 
   @CreateDateColumn()
