35c69dbcd28fd974b1c9d92aa663f0a416f7e2b1
william koller <wkoller25@gmail.com>
2021-01-20 01:24:02 -0300

    feat: set UserRepository in AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UserRepository } from 'src/user/repositories/user.repository';
+import { UserService } from 'src/user/services/user.service';
 import { AuthService } from './services/auth.service';
 
 @Module({
-  providers: [AuthService],
+  providers: [AuthService, UserService, UserRepository],
+  exports: [UserService],
 })
 export class AuthModule {}
