573cb3966ee794040d16f20240e82b76e9b4c72d
william koller <wkoller25@gmail.com>
2021-01-19 22:18:45 -0300

    chore: eslint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { configService } from './config/services/config.service';
+import { configService } from './config/config.service';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(configService.getTypeOrmConfig()[0]),
-    forwardRef(() => UserModule),
-  ],
+  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig()[0]), forwardRef(() => UserModule)],
   controllers: [],
   providers: [],
 })
