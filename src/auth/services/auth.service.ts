ac374cb8b9de06d9b6c90fd4a34326959f0a72c5
william koller <wkoller25@gmail.com>
2021-01-20 02:29:48 -0300

    feat: add jwttoken

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/entities/user.entity';
 import { UserService } from 'src/user/services/user.service';
+import { AuthInput } from '../dtos/auth.input';
+import { AuthType } from '../dtos/auth.type';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService, private readonly jwtService: JwtService) {}
 
-  async validateUser(username: string, password: string): Promise<any> {
-    const user = await this.userService.findOne(username);
-    if (user && user.password === password) {
-      const { password, ...result } = user;
-      return result;
-    }
-    return null;
+  async validateUser(data: AuthInput): Promise<AuthType> {
+    const user = await this.userService.findUserByEmail({ email: data.email });
+    const token = await this.jwtToken(user);
+    return {
+      user,
+      token,
+    };
+  }
+
+  private async jwtToken(user: User): Promise<string> {
+    const payload = {
+      username: user.username,
+      sub: user.id,
+    };
+    return this.jwtService.signAsync(payload);
   }
 }
