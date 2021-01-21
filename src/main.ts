766274bd571ce94f80fe473a688bca717853bd9a
william koller <wkoller25@gmail.com>
2021-01-21 00:04:27 -0300

    feat: set transform in ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('api');
   app.use(helmet());
   app.use(
