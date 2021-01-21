2ef0caf4fabfb19d742d5a97a9698364d0eea716
william koller <wkoller25@gmail.com>
2021-01-20 23:47:09 -0300

    fix: alter return JwtStrategy

--- src/auth/strategies/jwt/jwt.strategy.ts
+++ src/auth/strategies/jwt/jwt.strategy.ts
@@ -1,6 +1,8 @@
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
+@Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
@@ -10,10 +12,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any) {
-    return {
-      userId: payload.sub,
-      username: payload.username,
-    };
+  async validate(payload: any): Promise<any> {
+    return { userId: payload.sub, username: payload.username };
   }
 }
