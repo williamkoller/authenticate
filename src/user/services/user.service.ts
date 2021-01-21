30235ccf18bc20117fa3e355d54480184cb3860f
william koller <wkoller25@gmail.com>
2021-01-20 23:45:07 -0300

    refactor: add findUserByEmail with userRepository

--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -1,22 +1,28 @@
 import { BadGatewayException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
+import { Connection } from 'typeorm';
 import { CreateUserDto } from '../dtos/create-user.dto';
 import { UserRepository } from '../repositories/user.repository';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectRepository(User) private readonly userRepository: UserRepository) {}
+  constructor(
+    @InjectRepository(UserRepository) private readonly userRepository: UserRepository,
+    private readonly connection: Connection,
+  ) {
+    this.userRepository = this.connection.getCustomRepository(UserRepository);
+  }
 
   async create(data: CreateUserDto): Promise<User> {
     return await this.userRepository.createUser(data);
   }
 
-  async findUserByEmail(isEmail: { email: string }): Promise<User> {
-    const userEmail = await this.userRepository.findOne({ where: { email: isEmail.email } });
-    if (!userEmail) {
-      throw new NotFoundException('User not ound');
-    }
-    return userEmail;
+  async findUserByEmail(email: string): Promise<User> {
+    return await this.userRepository.findUserByEmail(email);
+  }
+
+  async findUserById(id: string): Promise<User> {
+    return await this.userRepository.findUserById(id);
   }
 }
