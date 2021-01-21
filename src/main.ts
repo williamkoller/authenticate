2fd0d6a873bcbbd0d3ba0f4251d5556be97c9657
william koller <wkoller25@gmail.com>
2021-01-20 22:08:25 -0300

    feat: add enableCors, rateLimit and helmet

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
+  app.use(helmet());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windows
+    }),
+  );
+  app.enableCors();
   await app.listen(process.env.PORT);
 }
 bootstrap();
