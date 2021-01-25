98cee5ec624479fa21d3fe14bf123da07f92340d
william koller <wkoller25@gmail.com>
2021-01-24 22:06:37 -0300

    refactor: call bcrypt on repository before save password

--- src/user/repositories/user.repository.ts
+++ src/user/repositories/user.repository.ts
@@ -1,24 +1,28 @@
-import { BadRequestException, InternalServerErrorException, NotFoundException } from '@nestjs/common';
-import { validateOrReject } from 'class-validator';
-import { User } from 'src/entities/user.entity';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+import { User } from '../models/user.entity';
 import { EntityRepository, Repository } from 'typeorm';
 import { CreateUserDto } from '../dtos';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(data: CreateUserDto): Promise<User> {
-    const user = this.create(data);
-    await validateOrReject(data);
-    const userSaved = await this.save(user);
-    delete userSaved.password;
-    if (!userSaved) {
-      throw new InternalServerErrorException('Error creating a user.');
+    const user = await this.findUserByEmail(data.email);
+    if (user) {
+      throw new BadRequestException('User exists with email.');
     }
+    const saltOrRounds = 10;
+    const hash = await bcrypt.hash(data.password, saltOrRounds);
+    const userSaved = await this.save({
+      email: data.email,
+      username: data.username,
+      password: hash,
+    });
     return userSaved;
   }
 
   async findUserByEmail(email: string): Promise<User> {
-    const emailUser = await this.findOne({ where: email });
+    const emailUser = await this.findOne({ email });
     if (!emailUser) {
       throw new NotFoundException('User not found.');
     }
@@ -26,7 +30,7 @@ export class UserRepository extends Repository<User> {
   }
 
   async findUserById(id: string): Promise<User> {
-    const userId = await this.findOne({ where: id });
+    const userId = await this.findOne({ where: { _id: id } });
     delete userId.password;
     if (!userId) {
       throw new BadRequestException('User not found.');
